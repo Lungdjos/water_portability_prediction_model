2b85ef2d2d0596fbcbffb27779997d51f0d000a3
Lungdjos <lungudjos@gmail.com>
2023-03-17 09:40:08 +0200

    loading the csv file for the dataset and checking for missig values

--- water_portability.py
+++ water_portability.py
@@ -6,3 +6,6 @@ data_set = pd.read_csv('water_portability.csv')
 # Split features and labels
 X = data_set.drop('Potability', axis=1)
 y = data_set['Potability']
+
+# Check for missing values
+print(X.isnull().sum())

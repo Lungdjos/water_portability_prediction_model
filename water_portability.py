61de0fd1fce0c070375323726e5cb14ba6b02833
Lungdjos <lungudjos@gmail.com>
2023-03-17 09:57:31 +0200

    spliting the loaded dataset into testing and training: setring the test size to 15% of the data

--- water_portability.py
+++ water_portability.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+# imports for model creation
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 # Load data from CSV
 data_set = pd.read_csv('water_portability.csv')
@@ -18,3 +21,8 @@ X = X.fillna(X.mean())
 # Standardize features
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
+
+# at this point the data preprocessing is done. we now create the model using Random Forest Algorithm
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
